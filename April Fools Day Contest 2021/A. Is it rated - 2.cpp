fea8666cd491adcc94053ae81bef375c2cdbda8e
rohit2000123sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-04-01 22:30:41 +0530

    Create A. Is it rated - 2.cpp

---
+++ April Fools Day Contest 2021/A. Is it rated - 2.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector< ll > vl;
+typedef pair< ll , ll > pl;
+typedef unordered_map< ll, ll > uml;
+typedef vector< pl > vpl;
+typedef map< ll, ll > ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    string str;
+    while(getline(cin,str)){    // Here we have used the simple getline function to take the whole line as the input string
+        cout<<"NO"<<endl;
+        cout.flush();
+    }
+
+    return 0;
+}
