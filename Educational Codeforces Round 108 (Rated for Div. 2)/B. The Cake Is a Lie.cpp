725d688c3354ae76977820f0864821b58bab2494
rohit2000123sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-04-30 19:10:16 +0530

    Create B. The Cake Is a Lie.cpp

---
+++ Educational Codeforces Round 108 (Rated for Div. 2)/B. The Cake Is a Lie.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector< ll > vl;
+typedef pair< ll , ll > pl;
+typedef unordered_map< ll, ll > uml;
+typedef vector< pl > vpl;
+typedef map< ll, ll > ml;
+
+ll calc(ll n, ll m){
+    ll ans=m-1;
+    ans+=m*(n-1);
+    return ans;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ll t;
+    cin>>t;
+
+    while(t--){
+        ll n,m,k;
+        cin>>n>>m>>k;
+
+        if(calc(n,m)==k){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+
+    return 0;
+}
