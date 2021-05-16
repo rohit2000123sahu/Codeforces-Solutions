6fba9c72a1b2bc3817209b10339965efc7dfa383
rohit2000123sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-05-16 22:58:52 +0530

    Create B. Phoenix and Puzzle.cpp

---
+++ Codeforces Global Round 14/B. Phoenix and Puzzle.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector< ll > vl;
+typedef pair< ll , ll > pl;
+typedef unordered_map< ll, ll > uml;
+typedef vector< pl > vpl;
+typedef map< ll, ll > ml;
+
+bool check(ll n ,ll val ){
+    if(n%val!=0){         
+        return false;
+    }
+    n=n/val;
+
+    ll temp=sqrt(n);
+
+    return temp*temp==n;        // Hence we are returning true if the n/val is perfect square else return false.
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ll t;
+    cin>>t;
+
+    while(t--){
+        ll n;
+        cin>>n;
+
+        if(n%2!=0){
+            cout<<"NO"<<endl;
+            continue;
+        }
+        if(check(n,2) || check(n,4)){     // One square could only be formed by using 2 or 4 squares only. It is also shown in the sample input output.
+            cout<<"YES"<<endl;            // Large sqaures will be the square of this small square only.
+        }else{
+            cout<<"NO"<<endl;
+        }
+
+
+    }
+
+    return 0;
+}
