f2977735812e9f041b5d67dab2cb5d9a9b229d53
rohit2000123sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-06-13 22:36:21 +0530

    Create F. Interesting Function.cpp

---
+++ Codeforces Round #725 (Div. 3)/F. Interesting Function.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll T;
+    cin >> T;
+
+    while (T--)
+    {
+        ll a, b;
+        cin >> a >> b;
+
+        ll ans = 0;
+
+        while (b > 0)
+        {
+            ans += (b - a);
+            b = b / 10;
+            a = a / 10;
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
