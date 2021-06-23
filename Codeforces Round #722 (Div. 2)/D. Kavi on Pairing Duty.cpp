9266aa08efc66490576340ebf5555fb8bd5568d8
rohit2000123sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-06-23 23:15:36 +0530

    Create D. Kavi on Pairing Duty.cpp

---
+++ Codeforces Round #722 (Div. 2)/D. Kavi on Pairing Duty.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 998244353
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll n;
+    cin >> n;
+
+    ll div[n + 1] = {0};
+    ll i, j;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = i + i; j <= n; j += i)
+        {
+            div[j]++;
+        }
+    }
+
+    ll dp[n + 1] = {0};
+    dp[0] = 1;
+    ll s = 1;
+
+    for (i = 1; i <= n; i++)
+    {
+        dp[i] = s + div[i];
+        dp[i] %= MOD;
+
+        s += dp[i];
+        s %= MOD;
+    }
+
+    cout << dp[n] << endl;
+
+    return 0;
+}
