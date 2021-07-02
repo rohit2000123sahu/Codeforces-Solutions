efd116cf3c9db99b97c8f11e220541d77af165eb
rohit2000123sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-07-02 21:51:54 +0530

    Create C. Challenging Cliffs.cpp

---
+++ Codeforces Round #726 (Div. 2)/C. Challenging Cliffs.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll T;
+    cin >> T;
+
+    while (T--)
+    {
+        ll n;
+        cin >> n;
+
+        ll a[n];
+        ll i;
+
+        for (i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        sort(a, a + n);
+
+        ll ans = 1e12;
+        ll start = -1;
+
+        for (i = 1; i < n; i++)
+        {
+            if (ans > a[i] - a[i - 1])
+            {
+                ans = a[i] - a[i - 1];
+                start = i - 1;
+            }
+        }
+
+        vl vect;
+
+        vect.pb(a[start]);
+
+        for (i = start + 2; i < n; i++)
+        {
+            vect.pb(a[i]);
+        }
+
+        for (i = 0; i < start; i++)
+        {
+            vect.pb(a[i]);
+        }
+
+        vect.pb(a[start + 1]);
+
+        for (auto c : vect)
+        {
+            cout << c << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
