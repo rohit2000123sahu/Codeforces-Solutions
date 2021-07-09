5cc0839fe463f40705f30869fea3126dd457b12c
rohit2000123sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-07-09 23:39:58 +0530

    Create C. Strange Function.cpp

---
+++ Codeforces Round #729 (Div. 2)/C. Strange Function.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll T;
+    cin >> T;
+
+    while (T--)
+    {
+        ll n;
+        cin >> n;
+
+        ll ans = 0;
+        ll num = 1;
+
+        for (ll i = 1; num <= n; ++i)
+        {
+            num = lcm(num, i);
+            if (num > n)
+            {
+                break;
+            }
+            ans += n / num;
+        }
+        ans += n;
+        ans = ans % MOD;
+        cout << ans << endl;
+    }
+
+    return 0;
+}
