5e6fa1c1c12ab5c45e767b5a5cc26acec1338109
rohit2000123sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-07-15 22:04:18 +0530

    Create A. AquaMoon and Two Arrays.cpp

---
+++ Codeforces Round #732 (Div. 2)/A. AquaMoon and Two Arrays.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll T;
+    cin >> T;
+
+    while (T--)
+    {
+        ll n;
+        cin >> n;
+
+        ll a[n], b[n];
+
+        ll i;
+
+        ll sum1 = 0, sum2 = 0;
+        for (i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            sum1 += a[i];
+        }
+
+        for (i = 0; i < n; i++)
+        {
+            cin >> b[i];
+            sum2 += b[i];
+        }
+
+        if (sum1 != sum2)
+        {
+            cout << -1 << endl;
+            continue;
+        }
+
+        i = 0;
+        ll j = 0;
+
+        vpl vect;
+
+        while (i < n && j < n)
+        {
+            if (a[i] > b[i])
+            {
+                while (j < n)
+                {
+                    if (a[j] < b[j])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        j++;
+                    }
+                }
+                vect.pb(mp(i + 1, j + 1));
+                a[i]--;
+                a[j]++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        cout << vect.size() << endl;
+
+        for (auto c : vect)
+        {
+            cout << c.F << " " << c.S << endl;
+        }
+    }
+
+    return 0;
+}
