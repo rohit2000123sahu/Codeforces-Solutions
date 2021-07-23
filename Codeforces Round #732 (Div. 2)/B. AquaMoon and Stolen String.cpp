ce0652c97d8e34b4bda353f56254c4110fabc2c9
rohit2000123sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-07-23 22:15:06 +0530

    Update B. AquaMoon and Stolen String.cpp

--- Codeforces Round #732 (Div. 2)/B. AquaMoon and Stolen String.cpp
+++ Codeforces Round #732 (Div. 2)/B. AquaMoon and Stolen String.cpp
@@ -35,58 +35,6 @@ int main()
 
         ll a[26] = {0};
 
-        for (i = 0; i < n; i++)
-        {
-            string temp;
-            cin >> temp;
-            vect.pb(temp);
-
-            for (auto c : temp)
-            {
-                a[c - 'a']++;
-            }
-        }
-
-        for (i = 0; i < n - 1; i++)
-        {
-            string temp;
-            cin >> temp;
-            for (auto c : temp)
-            {
-                a[c - 'a']--;
-            }
-        }
-
-        string ans = "";
-        i = 0;
-        while (i < 26)
-        {
-            if (a[i] <= 0)
-            {
-                i++;
-            }
-            else
-            {
-                ans += i + 'a';
-                a[i]--;
-            }
-        }
-
-        // cout << ans << endl;
-
-        for (i = 0; i < n; i++)
-        {
-            string temp = vect[i];
-            sort(all(temp));
-            if (temp == ans)
-            {
-                cout << vect[i] << endl;
-                break;
-            }
-        }
-
-        // cout << 1 << endl;
-        cout.flush();
     }
 
     return 0;
