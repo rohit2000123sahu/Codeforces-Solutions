a47d1b31f892361ef07b70bbf96239dbad729167
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-08-12 21:35:58 +0530

    Create A. Prison Break.cpp

---
+++ Codeforces Round #709 (Div. 2)/A. Prison Break.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll T;
+    cin >> T;
+
+    while (T--)
+    {
+        ll a, b;
+        cin >> a >> b;
+        cout << a * b << endl;
+    }
+
+    return 0;
+}
