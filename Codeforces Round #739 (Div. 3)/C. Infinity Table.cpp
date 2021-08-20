4b7b92fec2bfdba77e4be4fa9388243a781f33da
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-08-20 20:12:57 +0530

    Create C. Infinity Table.cpp

---
+++ Codeforces Round #739 (Div. 3)/C. Infinity Table.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll T;
+    cin >> T;
+
+    while (T--)
+    {
+        ll n;
+        cin >> n;
+
+        ll temp = sqrt(n);
+
+        if (temp * temp == n)
+        {
+            cout << temp << " " << 1 << endl;
+        }
+        else
+        {
+            ll left = n - temp * temp;
+            if (left <= temp + 1)
+            {
+                cout << left << " " << temp + 1 << endl;
+            }
+            else
+            {
+                cout << temp + 1 << " " << 2 * (temp + 1) - left << endl;
+            }
+        }
+    }
+
+    return 0;
+}
