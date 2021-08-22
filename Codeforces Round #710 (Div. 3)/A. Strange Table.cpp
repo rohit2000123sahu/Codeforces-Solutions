2a4527cf1072143677b122733a9c46cbe34f9d7b
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-08-22 22:36:40 +0530

    Create A. Strange Table.cpp

---
+++ Codeforces Round #710 (Div. 3)/A. Strange Table.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector< ll > vl;
+typedef pair< ll , ll > pl;
+typedef unordered_map< ll, ll > uml;
+typedef vector< pl > vpl;
+typedef map< ll, ll > ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ll t;
+    cin>>t;
+
+    while(t--){
+        ll n,m,x;
+        cin>>n>>m>>x;
+
+        ll col,row;
+
+        col= ceil(x/(double)n);
+
+        row = x-(col-1)*n;
+
+        cout<<(row-1)*m+col<<endl;  
+    }
+
+    return 0;
+}
