084c8631e608c35488ff99ec1ec044a3fc644079
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-09-06 20:31:01 +0530

    Update D1. Up the Strip (simplified version).cpp

--- Codeforces Round #740 (Div 2)/D1. Up the Strip (simplified version).cpp
+++ Codeforces Round #740 (Div 2)/D1. Up the Strip (simplified version).cpp
@@ -29,7 +29,7 @@ int main()
 
     dp[0] = dp[1] = 1;
 
-    ll pre[n + 1] = {0};
+    ll pre[n + 1] = {0};        // Prefix sum of dp used for the condition that it could jump to any location 1 to x-1 which will reduce O(n2) to O(n)
 
     pre[0] = pre[1] = 1;
 
@@ -38,21 +38,27 @@ int main()
     for (i = 2; i <= n; i++)
     {
         dp[i] = pre[i - 1];
+        // Second part where we should jump from x to x/z
+        // For this we will break the problem into two parts to reduce the complexity to O(n*sqrt(n))
+        
         ll ac = sqrt(i);
-        for (j = 2; j <= ac; j++)
+        for (j = 2; j <= ac; j++)       //Directly for all the numbers less than equal to sqrt(i)
         {
             dp[i] += dp[i / j];
             dp[i] %= m;
         }
+        
+        // See we will have only srqt(n) different values
+        // So in the below loop we will find that for how many different numbers we are having that gives that value.
         for (ll k = 1; k < i / ac; k++)
         {
             ll temp = 0;
-            temp = (i / k) - (i / (k + 1));
+            temp = (i / k) - (i / (k + 1));     // Total numbers that give k as the answer i.e. jump to k
             dp[i] += (dp[k] * temp) % m;
             dp[i] %= m;
         }
 
-        pre[i] = pre[i - 1] + dp[i];
+        pre[i] = pre[i - 1] + dp[i];            // Same as we have discussed above
         pre[i] %= m;
     }
 
