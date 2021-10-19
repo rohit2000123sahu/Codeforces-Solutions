89ef48541616e280968af844b26eb1c5f5470a28
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-10-19 23:11:07 +0530

    Update D1. All are Same.cpp

--- Codeforces Round #748 (Div. 3)/D1. All are Same.cpp
+++ Codeforces Round #748 (Div. 3)/D1. All are Same.cpp
@@ -36,7 +36,7 @@ int main()
             cin >> a[i];
         }
 
-        for (i = 1; i < n; i++)
+        for (i = 1; i < n; i++)     
         {
             if (a[i] != a[i - 1])
             {
@@ -44,17 +44,17 @@ int main()
             }
         }
 
-        if (i == n)
+        if (i == n)                 // If all are same then print -1
         {
             cout << -1 << endl;
         }
         else
         {
-            sort(a, a + n);
+            sort(a, a + n);     // Else we willl sort it 
             ll ans = 0;
             for (i = 0; i < n; i++)
             {
-                ans = __gcd(ans, a[i] - a[0]);
+                ans = __gcd(ans, a[i] - a[0]);     // Answer will be the gcd of the number subtract the smallest number
             }
             cout << ans << endl;
         }
