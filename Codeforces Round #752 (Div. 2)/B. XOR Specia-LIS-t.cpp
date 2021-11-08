5e23cba9e7afae02543b8ade6692b37fec8eb45f
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-11-08 21:16:41 +0530

    Create B. XOR Specia-LIS-t.cpp

---
+++ Codeforces Round #752 (Div. 2)/B. XOR Specia-LIS-t.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        ll a[n];
+        ll i;
+
+        for (i = 0; i < n; i++)
+            cin >> a[i];
+
+        if (n % 2 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            for (i = 1; i < n; i++)
+            {
+                if (a[i] <= a[i - 1])
+                {
+                    break;
+                }
+            }
+
+            if (i == n)
+            {
+                cout << "NO" << endl;
+            }
+            else
+            {
+                cout << "YES" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
