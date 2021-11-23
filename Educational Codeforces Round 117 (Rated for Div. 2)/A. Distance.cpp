e79f5f5d446618809b44fbc43c77dcf291a29aaf
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-11-23 22:18:56 +0530

    Create A. Distance.cpp

---
+++ Educational Codeforces Round 117 (Rated for Div. 2)/A. Distance.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x, y;
+        cin >> x >> y;
+        if ((x + y) % 2 == 0)
+        {
+            if (x % 2 == 0 && y % 2 == 0)
+            {
+                cout << x / 2 << " " << y / 2 << endl;
+            }
+            else
+            {
+                if (x > y)
+                {
+                    cout << (x - y) / 2 << " " << y << endl;
+                }
+                else
+                {
+                    cout << x << " " << (y - x) / 2 << endl;
+                }
+            }
+        }
+        else
+        {
+            cout << -1 << " " << -1 << endl;
+        }
+    }
+
+    return 0;
+}
