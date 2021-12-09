e273de4bc5fc89d9cf2021d0f28fc92022b6601f
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-12-09 19:24:59 +0530

    Create A. Anti Light's Cell Guessing.cpp

---
+++ Codeforces Global Round 17/A. Anti Light's Cell Guessing.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, m;
+        cin >> n >> m;
+
+        if (n == 1 && m == 1)
+        {
+            cout << 0 << endl;
+        }
+        else if (min(n, m) == 1)
+        {
+            cout << 1 << endl;
+        }
+        else
+        {
+            cout << 2 << endl;
+        }
+    }
+
+    return 0;
+}
