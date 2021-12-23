91da40c768102f0d1447a81f231cf53189c4965b
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-12-23 20:17:33 +0530

    Create A. Square String?.cpp

---
+++ Codeforces Round #762 (Div. 3)/A. Square String?.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        ll n = s.size();
+        if (n % 2 != 0)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            ll i;
+            for (i = 0; i < n / 2; i++)
+            {
+                if (s[i] != s[i + n / 2])
+                {
+                    break;
+                }
+            }
+
+            if (i == n / 2)
+            {
+                cout << "YES" << endl;
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
