b088d75403941becdb4f1ce22d5efb799692a4a2
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2021-12-30 23:14:10 +0530

    Create A. Construct a Rectangle.cpp

---
+++ Educational Codeforces Round 120 (Rated for Div. 2)/A. Construct a Rectangle.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+bool sum(ll a, ll b, ll c)
+{
+    if (a == b + c || b == a + c || c == a + b)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool equal(ll a, ll b, ll c)
+{
+    if (a == b && c % 2 == 0)
+    {
+        return true;
+    }
+    else if (b == c && a % 2 == 0)
+    {
+        return true;
+    }
+    else if (a == c && b % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b, c;
+        cin >> a >> b >> c;
+
+        if (sum(a, b, c) || equal(a, b, c))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
