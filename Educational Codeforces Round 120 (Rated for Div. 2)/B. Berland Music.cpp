d0db81f778f337e6e6886118259ed06636285fd1
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2022-01-08 21:56:58 +0530

    Create B. Berland Music.cpp

---
+++ Educational Codeforces Round 120 (Rated for Div. 2)/B. Berland Music.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        vector<ll> p(n);
+        ll i;
+
+        for (i = 0; i < n; i++)
+        {
+            cin >> p[i];
+        }
+
+        string s;
+        cin >> s;
+
+        vector<pair<ll, ll>> liked, disliked;
+        for (i = 0; i < n; i++)
+        {
+            if (s[i] == '1')
+            {
+                liked.push_back({p[i], i});
+            }
+            else
+            {
+                disliked.push_back({p[i], i});
+            }
+        }
+
+        sort(liked.begin(), liked.end(), greater<pair<ll, ll>>());
+        sort(disliked.begin(), disliked.end(), greater<pair<ll, ll>>());
+
+        ll rating = n;
+
+        vector<ll> q(n);
+
+        for (i = 0; i < liked.size(); i++)
+        {
+            q[liked[i].second] = rating;
+            rating--;
+        }
+
+        for (i = 0; i < disliked.size(); i++)
+        {
+            q[disliked[i].second] = rating;
+            rating--;
+        }
+
+        for (auto c : q)
+        {
+            cout << c << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
