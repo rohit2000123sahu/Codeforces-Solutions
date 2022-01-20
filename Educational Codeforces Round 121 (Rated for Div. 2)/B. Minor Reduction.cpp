a151d987114179893f3ce20c5c2eb0bd810cf584
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2022-01-20 20:27:05 +0530

    Create B. Minor Reduction.cpp

---
+++ Educational Codeforces Round 121 (Rated for Div. 2)/B. Minor Reduction.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        string num;
+        cin >> num;
+
+        ll i;
+        ll n = num.size();
+
+        bool done = false;
+
+        for (i = n - 2; i >= 0; i--)
+        {
+            ll a = num[i] - '0';
+            ll b = num[i + 1] - '0';
+
+            if (a + b >= 10)
+            {
+                num[i] = '0' + (a + b) / 10;
+                num[i + 1] = '0' + (a + b) % 10;
+                done = true;
+                break;
+            }
+        }
+
+        if (done)
+        {
+            cout << num << endl;
+        }
+        else
+        {
+            ll a = num[0] - '0';
+            ll b = num[1] - '0';
+
+            num[1] = a + b + '0';
+            num.erase(num.begin());
+            cout << num << endl;
+        }
+    }
+
+    return 0;
+}
