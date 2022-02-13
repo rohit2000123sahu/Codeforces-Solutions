35bc680dc421d8814b9d19a3288174c010db4dc1
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2022-02-13 19:41:53 +0530

    Create A. Sorting Parts.cpp

---
+++ Codeforces Global Round 19/A. Sorting Parts.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> vect(n);
+
+        ll i;
+        for (i = 0; i < n; i++)
+        {
+            cin >> vect[i];
+        }
+
+        vector<ll> sorted = vect;
+        sort(sorted.begin(), sorted.end());
+
+        if (vect == sorted)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+
+    return 0;
+}
