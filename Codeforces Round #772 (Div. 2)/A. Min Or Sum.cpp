d3f877ebe9bc7bc5416f16333fc440b647237970
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2022-02-21 21:55:29 +0530

    Create A. Min Or Sum.cpp

---
+++ Codeforces Round #772 (Div. 2)/A. Min Or Sum.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        vector<ll> vect(n);
+        ll i;
+
+        for (i = 0; i < n; i++)
+        {
+            cin >> vect[i];
+        }
+
+        ll bits[31] = {0};
+
+        for (auto c : vect)
+        {
+            i = 0;
+            while (c)
+            {
+                if (c & 1)
+                {
+                    bits[i]++;
+                }
+                c = c >> 1;
+                i++;
+            }
+        }
+
+        ll ans = 0;
+        ll pow = 1;
+        for (i = 0; i <= 30; i++)
+        {
+            if (bits[i] >= 1)
+            {
+                ans += pow;
+            }
+            pow = pow * 2;
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
