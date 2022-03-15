e1e90cbbb24163b067f70ea971714ba5ba49e939
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2022-03-15 22:54:34 +0530

    Create A. Madoka and Math Dad.cpp

---
+++ Codeforces Round #777 (Div. 2)/A. Madoka and Math Dad.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        ll count_1 = 0, count_2 = 0;
+        while (n > 0)
+        {
+            if (n >= 2)
+            {
+                count_2++;
+                n -= 2;
+            }
+            if (n >= 1)
+            {
+                count_1++;
+                n -= 1;
+            }
+        }
+
+        string ans;
+        if (count_2 >= count_1)
+        {
+            while (count_2 || count_1)
+            {
+                if (count_2)
+                {
+                    ans += '2';
+                    count_2--;
+                }
+                if (count_1)
+                {
+                    ans += '1';
+                    count_1--;
+                }
+            }
+        }
+        else
+        {
+            while (count_2 || count_1)
+            {
+                if (count_1)
+                {
+                    ans += '1';
+                    count_1--;
+                }
+                if (count_2)
+                {
+                    ans += '2';
+                    count_2--;
+                }
+            }
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
