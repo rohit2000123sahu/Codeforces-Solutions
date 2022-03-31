f87eada3c3da6c5fe19bc2b40d6ae62f8836df10
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2022-03-31 22:29:07 +0530

    Create B. Vlad and Candies.cpp

---
+++ Codeforces Round #780 (Div. 3)/B. Vlad and Candies.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef unordered_map<ll, ll> uml;
+typedef vector<pl> vpl;
+typedef map<ll, ll> ml;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+
+        vector<ll> a(n);
+        ll i;
+
+        for (i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        sort(all(a), greater<ll>());
+
+        if (n == 1)
+        {
+            if (a[0] > 1)
+            {
+                cout << "NO" << endl;
+            }
+            else
+            {
+                cout << "YES" << endl;
+            }
+        }
+        else
+        {
+            if (a[0] - a[1] <= 1)
+            {
+                cout << "YES" << endl;
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
