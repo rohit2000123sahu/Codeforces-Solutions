42ffebaed9ac5259910c387ffd7f3a8f19567425
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2022-04-24 22:56:36 +0530

    Create D. Colorful Stamp.cpp

---
+++ Codeforces Round #784 (Div. 4)/D. Colorful Stamp.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    ll i;
+    ll b = 0, r = 0;
+    ll w_index = -1;
+    for (i = 0; i < n; i++)
+    {
+        if (s[i] == 'B')
+        {
+            b++;
+        }
+        else if (s[i] == 'R')
+        {
+            r++;
+        }
+        else if (i >= 1 && s[i - 1] != 'W')
+        {
+            if (r == 0 || b == 0)
+            {
+                cout << "NO" << endl;
+                return;
+            }
+            r = 0;
+            b = 0;
+        }
+    }
+    if (s[n - 1] != 'W')
+    {
+        if (r == 0 || b == 0)
+        {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
