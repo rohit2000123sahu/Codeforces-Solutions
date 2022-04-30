294cb8fdebbec7fa8d24723bdbe8b8342f91ee3e
Rohit Sahu <59434806+rohit2000123sahu@users.noreply.github.com>
2022-04-30 20:54:18 +0530

    Create A. String Building.cpp

---
+++ Educational Codeforces Round 127 (Rated for Div. 2)/A. String Building.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define PI 3.1415926535897932384626
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define MOD 1000000007
+
+void solve()
+{
+    string s;
+    cin >> s;
+
+    ll i;
+    ll n = s.size();
+    ll count = 1;
+    for (i = 1; i < n; i++)
+    {
+        if (s[i] == s[i - 1])
+        {
+            count++;
+        }
+        else
+        {
+            if (count == 1)
+            {
+                cout << "NO" << endl;
+                return;
+            }
+            else
+            {
+                count = 1;
+            }
+        }
+    }
+
+    if (count == 1)
+    {
+        cout << "NO" << endl;
+        return;
+    }
+    cout << "YES" << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
